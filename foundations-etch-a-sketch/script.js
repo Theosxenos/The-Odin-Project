f5da7b0b2deb363b02c9508d7cd53e395dff70b2
TheosXenos <64856229+Theosxenos@users.noreply.github.com>
2023-02-09 11:52:32 +0100

    Finish Project

--- foundations-etch-a-sketch/script.js
+++ foundations-etch-a-sketch/script.js
@@ -1,11 +1,48 @@
 let containerdiv = document.querySelector(".container");
+let btnreset = document.querySelector("button");
 
-for (let rows = 1; rows<=255;rows++)
-{
+const containerwidth = containerdiv.offsetWidth;//.substring(0, -2);
 
-    let squarediv = document.createElement("div");
-    squarediv.className = "grid-item";
-    squarediv.textContent = rows;
+let squareamount = 16 * 16;
 
-    containerdiv.appendChild(squarediv);
+btnreset.addEventListener('click', btnResetClick);
+
+drawSqaures();
+
+function drawSqaures() {
+    const squarewidth = containerwidth / Math.sqrt(squareamount);
+
+    containerdiv.innerHTML = "";
+
+    for (let squares = 0; squares < squareamount; squares++) {
+
+        let squarediv = document.createElement("div");
+        squarediv.className = "grid-item";
+        squarediv.style.height = `${squarewidth}px`;
+        squarediv.style.width = `${squarewidth}px`;
+        // squarediv.textContent = squares;
+        squarediv.dataset.square = squares + 1;
+
+        squarediv.addEventListener('mouseenter', (e) => {
+            squarediv.classList.add('grid-item-hover');
+        });
+
+        containerdiv.appendChild(squarediv);
+    }
+}
+
+function btnResetClick(e) {
+    let input = prompt("Input a number");
+
+    if(input > 100)
+    {
+        alert('Please enter a number below 100.')
+        btnResetClick(e);
+
+        return;
+    }
+
+    squareamount = input * input;
+
+    drawSqaures();
 }
