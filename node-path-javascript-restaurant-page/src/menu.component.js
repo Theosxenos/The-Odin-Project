f23b4f779e7ef2a41c13dd54d87989c0ae335567
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2023-07-11 14:01:24 +0200

    Create menu.component.js

---
+++ node-path-javascript-restaurant-page/src/menu.component.js
@@ -0,0 +1,26 @@
+import {createElement} from "./helpers";
+
+const menuItemFactory = (name, price) => {
+    return {name, price}
+}
+
+const menu = [
+    menuItemFactory('Kolace', 1.33),
+    menuItemFactory('Babovka', 3.33),
+    menuItemFactory('Linecke', 0.33),
+    menuItemFactory('Vceli Uli', 2.33),
+]
+
+export default function createMenuComponent() {
+    const contentEl = document.querySelector('#content');
+    
+    const tableEl = createElement({tag: 'table', parent: contentEl});
+    const theadEl = createElement({tag: 'thead', parent: tableEl});
+    const tbodyEl = createElement({tag: 'tbody', parent: tableEl});
+
+    menu.forEach((menuitem) => {
+        let trEl = createElement({tag:'tr', parent: tbodyEl});
+        createElement({tag:'td', parent: trEl, innerHTML: menuitem.name});
+        createElement({tag:'td', parent: trEl, innerHTML: menuitem.price});
+    });
+}
